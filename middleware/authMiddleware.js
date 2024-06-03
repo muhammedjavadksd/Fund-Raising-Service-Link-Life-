4564f23933b60ca36493a3c22e2523694b774561
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-06-03 17:48:21 +0530

    User validation middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,46 @@
+const tokenHelper = require("../util/helper/tokenHelper");
 
 
 let authMiddleware = {
 
-    isValidUser: (req, res, next) => {
-        next()
+    isValidUser: async (req, res, next) => {
+
+        let headers = req.headers;
+
+        console.log(req.headers);
+        if (headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            if (!req.context) {
+                req.context = {}
+            }
+            let token = req.headers.authorization.split(' ')[1]
+            req.context.auth_token = token;
+            let checkValidity = await tokenHelper.checkTokenValidity(token)
+            if (checkValidity) {
+                console.log('Decode jwt is : ');
+
+                if (checkValidity?.email_id) {
+                    req.context.email_id = checkValidity?.email_id;
+                    req.context.token = token;
+                    console.log("Requested phone number is", checkValidity?.email_id);
+                    next()
+                } else {
+                    res.status(401).json({
+                        status: false,
+                        msg: "Authorization is failed"
+                    })
+                }
+            } else {
+                res.status(401).json({
+                    status: false,
+                    msg: "Authorization is failed"
+                })
+            }
+        } else {
+            res.status(401).json({
+                status: false,
+                msg: "Invalid auth attempt"
+            })
+        }
     },
 
     isValidAdmin: (req, res, next) => {
---
+++ util/helper/tokenHelper.js
@@ -0,0 +1,37 @@
+
+let jwt = require("jsonwebtoken")
+
+let tokenHelper = {
+    createJWTToken: async (payload = {}, timer) => {
+        try {
+            let jwtToken = await jwt.sign(payload, process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: timer });
+            console.log("The jwt token is");
+            return jwtToken
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    },
+
+    decodeJWTToken: async (jwttoken) => {
+        try {
+            let decode = await jwt.decode(jwttoken, { complete: true });
+            return decode
+        } catch (e) {
+            return null;
+        }
+    },
+
+    checkTokenValidity: async (token) => {
+        try {
+            let checkValidity = await jwt.verify(token, process.env.JWT_SECRET);
+            console.log("Token validity is : ");
+            console.log(checkValidity);
+            return checkValidity
+        } catch (e) {
+            return false
+        }
+    }
+}
+
+module.exports = tokenHelper;
